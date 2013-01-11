53f968e49df9707d2d749a749ae4e362f6ffdfda
wang lifeng <wang.lifeng@careerbuilder.com>
2013-01-11 16:52:42 -0500

    create prj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wechat::Application.config.secret_token = '2c54ab1495f92acc2af745dd962038d29c5706fb25415d041d29826c634b5c8c2d857349e4f30a7dd7657cd3defc79fcb76ddd0552541d13d92c0df4a4c9f813'
